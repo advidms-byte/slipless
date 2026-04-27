2ad048b57267de726ef7644aad2e4194abc34737
advidms-byte <advidms@gmail.com>
2026-04-28 01:30:43 +0530

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import { createClient as createBrowserClient } from '@supabase/supabase-js'
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
+
+export function createAdminClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  )
+}
